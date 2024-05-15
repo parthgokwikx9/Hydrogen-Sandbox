ca02cc9d617679ae1dfe5479ee7c763d69b9d7ce
partharorax9 <parth.arora@gokwik.co>
2024-05-15 18:29:00 +0530

    added gokwik configs

--- app/gokwik/gokwik.config.ts
+++ app/gokwik/gokwik.config.ts
@@ -1,6 +1,6 @@
 export const gokwikConfig = {
-  mid: '19g6jle2d5p3n',
-  env: 'sandbox',
-  storefrontAccessToken: '35f46317841d6f44370c876c47b4016e',
-  shopifyGraphQlUrl: `https://sandbox-plus-gokwik.myshopify.com/api/2024-01/graphql.json`,
+  mid: '19g6ilw7j9yoz',
+  env: 'production',
+  storefrontAccessToken: '40049fcb26445eaa9999fd7519cc1131',
+  shopifyGraphQlUrl: `https://qa-plus-gokwik.myshopify.com/api/2024-01/graphql.json`,
 };
